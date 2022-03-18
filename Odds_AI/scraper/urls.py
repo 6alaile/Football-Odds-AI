43786347de754e7fe22832aa521d72b11c663ae4
wilby-mj <23701282+wilby-mj@users.noreply.github.com>
2022-03-18 10:40:56 +0300

    Installed Selenium

--- Odds_AI/scraper/urls.py
+++ Odds_AI/scraper/urls.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('/scraper', views.scraper, name="scraper"),
+    path('', views.default, name="default"),
 ]
