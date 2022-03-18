d5f6a23ee33bf997b935f00e7d76109e7aa3c691
wilby-mj <23701282+wilby-mj@users.noreply.github.com>
2022-03-18 23:27:54 +0300

    Setup selenium in Django

--- Odds_AI/scraper/views.py
+++ Odds_AI/scraper/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+import os
+
+# Selenium
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+def scrap(request):
+    options = Options()
+    options.headless = True
+    options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Chrome(options=options)
 
 # Create your views here.
 
