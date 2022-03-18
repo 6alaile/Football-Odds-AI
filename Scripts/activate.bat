3124666b3801be81d2dff964d9679706ba7a72cc
wilby-mj <23701282+wilby-mj@users.noreply.github.com>
2022-03-18 09:32:28 +0300

    Update venv paths

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\wbala\Documents\Github\Football-API
+set VIRTUAL_ENV=C:\Users\wbala\Documents\Github\Football-Odds-AI
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(Football-API) %PROMPT%
+set PROMPT=(Football-Odds-AI) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(Football-API) 
+set VIRTUAL_ENV_PROMPT=(Football-Odds-AI) 
 
 :END
 if defined _OLD_CODEPAGE (
